b52055252851c1e546fb201588b26f5dbae13432
unenlightened <adept_inept@yahoo.com>
2018-10-09 14:48:12 -0700

    block out Projects components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
+import Projects from './containers/Projects'  
+
+const projects = [
+  {
+    name: "Very First Project",
+    description: "Hello World!",
+    img_url: "https://media.giphy.com/media/2xPPojqe3mraUXS6dk/giphy.gif"
+  },
+  {
+    name: "MyFace",
+    description: "The next big social media platform.",
+    img_url: "https://media.giphy.com/media/14cilFdQzr8hG0/giphy.gif"
+  }
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Whee!
+        <Projects projects={projects} />
       </div>
     );
   }
---
+++ src/containers/Projects.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Projects = ({ projects }) => {
+  return (
+    <div className="ProjectList">
+      <h1>Projects</h1>
+      {projects.map(project=> 
+        <div className="ProjectCard">
+          <h3>{project.name}</h3>
+          <img src={project.img_url} alt={project.name} />
+          <p>{project.description}</p>
+          <br />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Projects
+
