b10209d56071aff8d15985ec42a183304179d1b8
unenlightened <adept_inept@yahoo.com>
2018-10-09 12:03:45 -0700

    add Project seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ 
+projects = Project.create([{
+  name: "Very First Project",
+  description: "Hello World!",
+  img_url: nil
+},
+{
+  name: "MyFace",
+  description: "The next big social media platform.",
+  img_url: nil
+},
+{
+  name: "ChitCoin",
+  description: "Cryptocurrency currently valued at $0.0000000001",
+  img_url: nil
+},
+{
+  name: "Skynet",
+  description: "Artificial intelligence and machine learning to make the future a better place.",
+  img_url: nil
+},
+{
+  name: "Matrix",
+  description: "MMO with direct interface. The immersion is way more realistic.",
+  img_url: nil
+}])
+ 
