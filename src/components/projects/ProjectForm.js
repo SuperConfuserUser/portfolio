b2ca91c795769390d3a63cb13c5a10362743ded1
unenlightened <adept_inept@yahoo.com>
2018-10-10 20:10:17 -0700

    Block basic project form

--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -2,18 +2,62 @@ import React, { Component } from 'react'
 
 export class ProjectForm extends Component {
 
+  state = {
+    name: '',
+    img_url: '',
+    description: ''
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault()
+    this.setState({
+      name: '',
+      img_url: '',
+      description: ''
+    })
+    console.log(this.state)
+  }
 
   render() {
+    const { name, img_url, description } = this.state
+
     return (
       <div>
         <h2>Add New Project</h2>
-        <form>
-          <label>Name</label><br />
-          <input type="text" name="name" /><br />
-          <label>Image URL</label><br />
-          <input type="text" name="img_url" /><br />
-          <label>Description</label><br />
-          <textarea name="description" /><br />
+        <form onSubmit={this.handleOnSubmit}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input 
+              type="text" 
+              name="name"
+              onChange={this.handleOnChange}
+              value={name}
+            />
+          </div>
+          <div>
+            <label htmlFor="img_url">Image URL</label>
+            <input 
+              type="text" 
+              name="img_url"
+              onChange={this.handleOnChange}
+              value={img_url}
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description</label>
+            <textarea 
+              name="description"
+              onChange={this.handleOnChange}
+              value={description}
+            />
+          </div>
+          <br />
           <input type="submit" value="Add" />
         </form>
       </div>
