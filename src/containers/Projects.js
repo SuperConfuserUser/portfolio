fa06ba104332c49f9b0a91a9aef2fd499ef0fc8a
unenlightened <adept_inept@yahoo.com>
2018-10-10 19:12:28 -0700

    Add ProjectCard component to Project container

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,17 +1,11 @@
 import React from 'react'
+import ProjectCard from '../components/ProjectCard'
 
 const Projects = ({ projects }) => {
   return (
     <div className="ProjectList">
       <h1>Projects</h1>
-      {projects.map(project=> 
-        <div key={project.id} className="ProjectCard">
-          <h3>{project.name}</h3>
-          <img src={project.img_url} alt={project.name} />
-          <p>{project.description}</p>
-          <br />
-        </div>
-      )}
+      {projects.map(project=> <ProjectCard key={project.id} project={project} />)}
     </div>
   )
 }
