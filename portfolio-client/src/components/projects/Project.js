5f1b08e8127304979a4750dd50693ea266a4df61
unenlightened <adept_inept@yahoo.com>
2018-10-18 22:16:31 -0700

    Add project hidden route behavior

--- portfolio-client/src/components/projects/Project.js
+++ portfolio-client/src/components/projects/Project.js
@@ -1,35 +1,19 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { getProject, resetProject } from '../../actions/projectActions'
+import React from 'react'
 import ProjectAdminButtons from './ProjectAdminButtons'
 
-class Project extends Component {
-
-  constructor(props) {
-    super(props)
-    this.props.resetProject()
-  }
-
-  componentDidMount() {
-    const { getProject, match } = this.props
-    getProject(match.params.projectId)
-  }
+function Project(props) {
+  const { project } = props
   
-  render() {
-    const { project } = this.props
+  return (
+    <div>
+      <ProjectAdminButtons project={project} />
 
-    return(
-      <div>
-        <ProjectAdminButtons project={project} />
-
-        <h2>{project.name}</h2>
-        <img src={project.img_url} alt={project.name} />
-        <p>{project.description}</p>
-      </div>
-    )
-  }
+      <h2>{project.name}</h2>
+      <img src={project.img_url} alt={project.name} />
+      <p>{project.description}</p>
+    </div>
+  )
 }
 
-const mapStateToProps = ({ project }) => ({ project })
+export default Project
 
-export default connect(mapStateToProps, { getProject, resetProject })(Project)
---
+++ portfolio-client/src/components/projects/ProjectContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Route, Redirect } from 'react-router-dom'
+import { getProject, resetProject } from '../../actions/projectActions'
+import Project from './Project'
+
+export class ProjectContainer extends Component {
+
+  componentDidMount() {
+    const { getProject, match, history, admin } = this.props
+    getProject(match.params.projectId)
+  }
+
+  componentWillUnmount() {
+    this.props.resetProject()
+  }
+
+  render() {
+    const { match, project, admin } = this.props
+
+    return (
+      <div>
+        <HiddenRoute exact path={match.path} component={Project} project={project} admin={admin} />
+      </div>
+    )
+  }
+}
+
+const HiddenRoute = ({ component: Component, project: project, admin, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    project.hidden ?
+    <Redirect to='/projects' /> :
+    <Component {...props} project={project} />
+  )} />
+)
+
+const mapStateToProps = ({ project, admin }) => ({ project, admin })
+
+export default connect(mapStateToProps, { getProject, resetProject })(ProjectContainer)
