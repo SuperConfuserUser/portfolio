902fdf5e7e9c33d024e70624ec2a4a28e7d99f7e
unenlightened <adept_inept@yahoo.com>
2018-10-19 15:16:47 -0700

    Add redux-persist to save to admin options to localstorage

--- portfolio-client/src/index.js
+++ portfolio-client/src/index.js
@@ -2,14 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker'
+import './index.css'
 
 import App from './components/App'
-import store from './store/configureStore'
-import './index.css'
+import { PersistGate } from 'redux-persist/integration/react'
+import configureStore from './store/configureStore'
+const { persistor, store } = configureStore()
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>, 
 document.getElementById('root'))
 
--- portfolio-client/src/store/configureStore.js
+++ portfolio-client/src/store/configureStore.js
@@ -1,18 +1,29 @@
-import {
-  createStore,
-  applyMiddleware,
-  compose
-} from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers/rootReducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 const middleware = [thunk]
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer,
-  composeEnhancers(
-    applyMiddleware(...middleware)
-  )
-)
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['admin']
+}
 
-export default store
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+export default () => {
+  const store = createStore(
+    persistedReducer,
+    composeEnhancers(
+      applyMiddleware(...middleware)
+    )
+  )
+  const persistor = persistStore(store)
+  return { store, persistor }
+}
