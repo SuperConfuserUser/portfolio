9a887cc1dadafce8b81e72eba4e39302ecb80b76
unenlightened <adept_inept@yahoo.com>
2018-10-26 12:35:30 -0700

    Add user model validations and reset database

--- portfolio-api/app/models/user.rb
+++ portfolio-api/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates_format_of :email, :with => /.+@.+\..+/i
 end
