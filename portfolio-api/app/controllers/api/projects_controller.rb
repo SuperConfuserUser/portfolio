017a2ef38bb6f813c88ce86a804a0bec71a9e6f6
unenlightened <adept_inept@yahoo.com>
2018-10-26 14:18:32 -0700

    Add knock gem and update configuration for user authentication

--- portfolio-api/app/controllers/api/projects_controller.rb
+++ portfolio-api/app/controllers/api/projects_controller.rb
@@ -1,48 +1,48 @@
 class Api::ProjectsController < ApplicationController
 
-    before_action :set_project, only: [:show, :update, :destroy]
+  before_action :set_project, only: [:show, :update, :destroy]
 
-    def index
-      render json: Project.all
+  def index
+    render json: Project.all
+  end
+
+  def show
+    render json: @project
+  end
+
+  def create
+    project = Project.new(project_params)
+    if project.save
+      render json: project
+    else
+      render json: { errors: project.errors }, status: 422
     end
+  end
 
-    def show
+  def update
+    if @project.update(project_params)
       render json: @project
+    else
+      render json: { errors: @project.errors }, status: 422
+    end
+  end
+
+  def destroy
+    if @project.destroy
+      render json: { message: "Project successfully deleted" }, status: 200
+    else
+      render json: { error: "Unable to delete"}, status: 400
+    end
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:name, :description, :img_url, :hidden)
     end
 
-    def create
-      project = Project.new(project_params)
-      if project.save
-        render json: project
-      else
-        render json: { errors: project.errors }, status: 422
-      end
+    def set_project
+      @project = Project.find(params[:id])
     end
-
-    def update
-      if @project.update(project_params)
-        render json: @project
-      else
-        render json: { errors: @project.errors }, status: 422
-      end
-    end
-
-    def destroy
-      if @project.destroy
-        render json: { message: "Project successfully deleted" }, status: 200
-      else
-        render json: { error: "Unable to delete"}, status: 400
-      end
-    end
-
-    private
-
-      def project_params
-        params.require(:project).permit(:name, :description, :img_url, :hidden)
-      end
-
-      def set_project
-        @project = Project.find(params[:id])
-      end
 
 end
--- portfolio-api/config/routes.rb
+++ portfolio-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :projects, except: [:new, :edit]
-
     post 'contact', to: 'messages#create', as: 'create_message'
+    post 'user_token', to: 'user_token#create'
   end
 end
