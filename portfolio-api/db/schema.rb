8e71cefb3ef0cbd41d527514670c3e9b5f5684dc
unenlightened <adept_inept@yahoo.com>
2018-10-26 12:27:47 -0700

    Add User model with rails generate

--- portfolio-api/db/schema.rb
+++ portfolio-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_033553) do
+ActiveRecord::Schema.define(version: 2018_10_26_190508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2018_10_17_033553) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "hidden", default: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
