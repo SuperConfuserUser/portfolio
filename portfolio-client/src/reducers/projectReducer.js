aa6eeff60f65238e5f66e871ae0f4148b337c948
unenlightened <adept_inept@yahoo.com>
2018-10-25 21:32:11 -0700

    Add UPDATE_PROJECT_SUCCESS type to projectReducer

--- portfolio-client/src/reducers/projectReducer.js
+++ portfolio-client/src/reducers/projectReducer.js
@@ -11,6 +11,9 @@ export default (state = initialState, action) => {
     case 'GET_PROJECT_SUCCESS':
       return action.project
 
+    case 'UPDATE_PROJECT_SUCCESS':
+      return action.project
+
     case 'RESET_PROJECT':
       return initialState
 
