cc09d4b3c4154524477e6e8a324a6b2c0d9c7c0c
unenlightened <adept_inept@yahoo.com>
2018-10-31 20:00:48 -0700

    Link Font Awesome and add rel prop to external links

--- portfolio-client/src/components/Footer.js
+++ portfolio-client/src/components/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
         © 2018 Chely Ho
       </div>
       <div className='external-links'>
-        <a href='https://github.com/unenlightened' target='_blank'><i className='fab fa-github'></i></a>
-        <a href='https://www.linkedin.com/in/chelyho/' target='_blank'><i className='fab fa-linkedin' target='_blank'></i></a>
-        <a href='mailto: hello@chelyho.com' target='_blank'><i className='fas fa-envelope'></i></a>
+        <a href='https://github.com/unenlightened' target='_blank' rel="noopener noreferrer"><i className='fab fa-github'></i></a>
+        <a href='https://www.linkedin.com/in/chelyho/' target='_blank' rel="noopener noreferrer"><i className='fab fa-linkedin' target='_blank'></i></a>
+        <a href='mailto: hello@chelyho.com'><i className='fas fa-envelope'></i></a>
         {/* <a><i className='material-icons'>mail</i></a> */}
 
         {/* TODO: on hove expand out the names for med and large screen sizes */}
