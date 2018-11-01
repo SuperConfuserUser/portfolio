9d140339666faf3060ca0cc94372f73a24cd6edc
unenlightened <adept_inept@yahoo.com>
2018-11-01 12:44:37 -0700

    Cleanup duplicate propType declaration

--- portfolio-client/src/components/contact/ContactForm.js
+++ portfolio-client/src/components/contact/ContactForm.js
@@ -3,12 +3,6 @@ import PropTypes from 'prop-types'
 
 class ContactForm extends Component {
 
-  static propTypes = {
-    contactFormData: PropTypes.object.isRequired,
-    updateMessage: PropTypes.func.isRequired,
-    sendMessage: PropTypes.func.isRequired,
-  }
-
   constructor(props) {
     super(props)
     this.state = {
