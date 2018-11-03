40b5d1bffa521bef075644a761f5dfd95ac6f476
unenlightened <adept_inept@yahoo.com>
2018-11-03 11:09:51 -0700

    Cleanup Footer and update icons

--- portfolio-client/src/components/Footer.js
+++ portfolio-client/src/components/Footer.js
@@ -7,16 +7,18 @@ function Footer() {
         © 2018 Chely Ho
       </div>
       <div className='external-links'>
-        <a href='https://github.com/unenlightened' target='_blank' rel="noopener noreferrer"><i className='fab fa-github'></i></a>
-        <a href='https://www.linkedin.com/in/chelyho/' target='_blank' rel="noopener noreferrer"><i className='fab fa-linkedin' target='_blank'></i></a>
-        <a href='mailto: hello@chelyho.com'><i className='fas fa-envelope'></i></a>
-        {/* <a><i className='material-icons'>mail</i></a> */}
+        <a href='https://github.com/unenlightened' target='_blank' rel="noopener noreferrer">
+          <i className='fab fa-github'></i>
+        </a>
+        <a href='https://www.linkedin.com/in/chelyho/' target='_blank' rel="noopener noreferrer">
+         <i className='fab fa-linkedin-in' target='_blank'></i>
+        </a>
+        <a href='mailto: hello@chelyho.com'>
+          <i className='far fa-envelope'></i>
+        </a>
 
         {/* TODO: on hove expand out the names for med and large screen sizes */}
-        {/* extra margin coming from somewhere.... maybe sticky footer */}
-        {/* <a>GitHub</a>
-        <a>LinkedIn</a>
-        <a>Email</a> */}
+        {/* TODO: extra margin coming from somewhere.... maybe sticky footer */}
       </div> 
     </footer>
   )
