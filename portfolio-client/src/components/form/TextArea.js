1dc7463f98a26589b708666bab360848215f9de2
unenlightened <adept_inept@yahoo.com>
2018-11-03 23:21:25 -0700

    Refactor TextArea and change class to function

--- portfolio-client/src/components/form/TextArea.js
+++ portfolio-client/src/components/form/TextArea.js
@@ -1,51 +1,46 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class TextArea extends Component {
-
-  render() {
-    const { klass, id, value, children, handleChange, errors, editMode, innerRef } = this.props
+const TextArea = React.forwardRef((props, ref) => {
+  const { klass, id, value, children, handleChange, errors, editMode } = props
   
-    const error = Object.keys(errors).length ? 
-      (errors[id] ? errors[id] : '') : ''
+  const error = Object.keys(errors).length ? 
+    (errors[id] ? errors[id] : '') : ''
 
-    const inputKlass = error ? 'materialize-textarea invalid' : 'materialize-textarea'
-    const labelKlass = editMode && value ? (id + ' active') : id
+  const inputKlass = 'materialize-textarea' + (error ?  ' invalid' : '')
+  const labelKlass = editMode && value ? (id + ' active') : id
 
-    return (  
-      <div className={klass}>
-        <div className='input-field'>
-          <textarea 
-            id={id}
-            onChange={handleChange}
-            value={value}
-            className={inputKlass}
-            ref={innerRef}
-          />
-          <label htmlFor={id} className={labelKlass}>{children}</label>
-          {error && 
-            <span data-error={error}></span>}
-        </div>
+  return (  
+    <div className={klass}>
+      <div className='input-field'>
+        <textarea 
+          id={id}
+          onChange={handleChange}
+          value={value}
+          className={inputKlass}
+          ref={ref}
+        />
+        <label htmlFor={id} className={labelKlass}>{children}</label>
+        {error && 
+          <span data-error={error}></span>}
       </div>
-    )
-  }
+    </div>
+  )
+})
 
-  static propTypes = {
-    klass: PropTypes.string,
-    id: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    handleChange: PropTypes.func.isRequired,
-    errors: PropTypes.object,
-    editMode: PropTypes.bool
-  }
-
-  static defaultProps = {
-    klass: 'text-area',
-    errors: {},
-    editMode: false,
-  }
+TextArea.propTypes = {
+  klass: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  errors: PropTypes.object,
+  editMode: PropTypes.bool
 }
 
-export default React.forwardRef((props, ref) => 
-  <TextArea innerRef={ref} {...props} />
-)
+TextArea.defaultProps = {
+  klass: 'text-area',
+  errors: {},
+  editMode: false,
+}
+
+export default TextArea
