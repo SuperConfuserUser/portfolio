6d1bf9e607d352f01078fef74115d97af9330e76
unenlightened <adept_inept@yahoo.com>
2018-11-05 18:30:04 -0800

    Add DetailLinkList and placeholder data

---
+++ portfolio-client/src/components/projects/DetailLinkList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function DetailLinkList(props) {
+  const { links } = props
+  
+  const renderLinks = links.map(link => 
+    <li>
+      <a href={link.url} alt={link.name} target='_blank' rel="noopener noreferrer">{link.name}</a>
+    </li>
+  )
+  return (
+    <ul className='link-list'>
+      {renderLinks}
+    </ul>
+  )
+}
+
+DetailLinkList.propTypes = {
+  links: PropTypes.array.isRequired
+}
+
+export default DetailLinkList
+
--- portfolio-client/src/components/projects/Project.js
+++ portfolio-client/src/components/projects/Project.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import DetailLinkList from './DetailLinkList'
 
 function Project(props) {
   const { name, img_url, description, created_at } = props.project
@@ -10,7 +11,20 @@ function Project(props) {
   })
 
   //TODO: add category option for Rails side to pass to client-side
+  //TODO: add links to Rails side
+  
   const category = 'Website'
+
+  const content = `<p>Lorem ipsum dolor sit amet, <em>consectetur</em> adipiscing elit. Fusce a consectetur est. Vestibulum porta non purus in consequat. Donec vitae tempus eros. Cras non vulputate neque. Proin id quam sed diam volutpat euismod sed ut nunc. Sed et sapien ac ligula feugiat cursus.</p>
+
+  <p>Nullam mauris diam, <em>Ruby</em> maximus eu purus maximus, imperdiet lacinia lorem. Donec consectetur risus eros, et viverra neque aliquam vitae. Praesent lacus elit, dapibus a augue tincidunt, pharetra ullamcorper sapien. Sed vulputate rhoncus ornare. Integer non magna ac sem lacinia molestie. Sed et volutpat tortor. Pellentesque <em>facilisis</em> ex ac libero condimentum ultrices in a libero. Proin a elit eros. Vestibulum vel porttitor dolor. Nam placerat eros vel tincidunt cursus.</p>`
+
+  const links = [
+    { name: 'Demo', url: '#' },
+    { name: 'Github', url: 'https://github.com/unenlightened/' },
+    { name: 'Walkthrough', url: 'https://www.youtube.com/watch?v=Q0AULj4UltI' }
+  ]
+ 
 
   return (
     <div className='project-detail'>
@@ -23,15 +37,9 @@ function Project(props) {
 
       <h5>{description}</h5>
 
-      <p>Lorem ipsum dolor sit amet, <em>consectetur</em> adipiscing elit. Fusce a consectetur est. Vestibulum porta non purus in consequat. Donec vitae tempus eros. Cras non vulputate neque. Proin id quam sed diam volutpat euismod sed ut nunc. Sed et sapien ac ligula feugiat cursus.</p>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
 
-      <p>Nullam mauris diam, <em>Ruby</em> maximus eu purus maximus, imperdiet lacinia lorem. Donec consectetur risus eros, et viverra neque aliquam vitae. Praesent lacus elit, dapibus a augue tincidunt, pharetra ullamcorper sapien. Sed vulputate rhoncus ornare. Integer non magna ac sem lacinia molestie. Sed et volutpat tortor. Pellentesque <em>facilisis</em> ex ac libero condimentum ultrices in a libero. Proin a elit eros. Vestibulum vel porttitor dolor. Nam placerat eros vel tincidunt cursus.</p>
-
-      <ul className='link-list'>
-        <li><a>Demo</a></li>
-        <li><a>Walkthrough</a></li>
-        <li><a>Github</a></li>
-      </ul>
+      <DetailLinkList links={links} />
 
     </div>
   )
